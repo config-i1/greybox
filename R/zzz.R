c9f2b315b173fe1589930f4cfc646e37cd949e02
Ivan Svetunkov <ivan@svetunkov.ru>
2019-04-19 21:07:12 +0100

    Dynamic unload for the package

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,7 @@
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage(paste0("Package \"greybox\", v",packageVersion(pkgname)," loaded."));
 }
+
+.onUnload <- function (libpath) {
+  library.dynam.unload("greybox", libpath)
+}
