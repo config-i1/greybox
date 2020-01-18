631c9130f9d76f81ff4ca352b2fd1754628dba1d
Ivan Svetunkov <ivan@svetunkov.ru>
2020-01-18 14:34:54 +0000

    A function for Intermittent Demand identification

---
+++ R/intermittentDemandIdentificator.R
@@ -0,0 +1,15 @@
+identifyID <- function(y, ic="AIC"){
+
+    if(all(y!=0) | all(y==0)){
+        retrn(FALSE);
+    }
+
+    # First model with normal distribution
+    model1 <- alm(y~1, data.frame(y=y), distribution="dnorm");
+    # The second model with IG
+    model2 <- alm(y~1, data.frame(y=y), distribution="dinvgauss", occurrence="plogis")
+    # Fix the number of parameters
+    model2$df <- 3;
+
+    return(AIC(model2)<AIC(model1))
+}
