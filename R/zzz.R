75f6483272b29e5b59c9df1b9e1e33141f463699
Ivan Svetunkov <ivan@svetunkov.ru>
2021-02-23 12:59:57 +0000

    Experimenting with colours

--- R/zzz.R
+++ R/zzz.R
@@ -1,20 +1,24 @@
 .onAttach <- function(libname, pkgname) {
     startUpMessage <- paste0("Package \"greybox\", v",packageVersion(pkgname)," loaded.");
-    randomNumber <- ceiling(runif(1,0,10));
-    if(randomNumber==1){
+    randomNumber <- ceil(runif(1,0,10));
+    # randomNumber <- trunc(runif(1,1,101));
+    if(randomNumber<=4){
+      startUpMessage <- paste0(startUpMessage,"\n\033[38;2;00;70;20m");
+      if(randomNumber==1){
         startUpMessage <- paste0(startUpMessage,"\nBy the way, have you already tried temporaldummy() function from greybox?");
-    }
-    else if(randomNumber==2){
+      }
+      else if(randomNumber==2){
         startUpMessage <- paste0(startUpMessage,"\nIf you want to know more about the greybox and forecasting, ",
                                  "you can visit my website: https://forecasting.svetunkov.ru/");
-    }
-    else if(randomNumber==3){
+      }
+      else if(randomNumber==3){
         startUpMessage <- paste0(startUpMessage,"\nAny thought or suggestions about the package? ",
                                  "Have you found a bug? File an issue on github: https://github.com/config-i1/greybox/issues");
-    }
-    else if(randomNumber==4){
+      }
+      else if(randomNumber==4){
         startUpMessage <- paste0(startUpMessage,"\nDid you know that you can use your own loss function in alm()? ",
                                  "This is regulated with 'loss' parameter. See documentation for examples.");
+      }
     }
     startUpMessage <- paste0(startUpMessage,"\n");
     packageStartupMessage(startUpMessage);
