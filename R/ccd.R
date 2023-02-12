4bdd80f15c497ee2263b2d679bb959d745d992a2
Ivan Svetunkov <ivan@svetunkov.ru>
2023-02-12 17:32:25 +0000

    Draft of Cycliacal Coordinate Descent function

---
+++ R/ccd.R
@@ -0,0 +1,22 @@
+# ccd <- function(B, BUpper, BLower, CF, opts, ...){
+#     # Function implements a simple Cyclical Coordinate Descent
+#
+#     CFLocal <- function(b){
+#         B[j] <- b;
+#         return(CF(B, ...));
+#     }
+#
+#     niter <- 10
+#     for(i in 1:niter){
+#         for(j in 1:length(B)){
+#             res <- optimise(CFLocal, lower=BLower, upper=BUpper)
+#             B[j] <- res$minimum;
+#             # res <- nloptr(B[j], CFLocal,
+#             #               opts=opts,
+#             #               lb=BLower[j], ub=BUpper[j]);
+#             # B[j] <- res$solution;
+#             CFValue <- res$objective;
+#         }
+#     }
+#     return(list(solution=B,objective=CFValue));
+# }
