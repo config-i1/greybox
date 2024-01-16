0141e87c4e69d02467df5dc9d66c9e1239d45f99
Ivan Svetunkov <i.svetunkov@lancaster.ac.uk>
2024-01-16 12:05:26 +0000

    Some tuning

--- R/intermittentDemandIdentifier.R
+++ R/intermittentDemandIdentifier.R
@@ -1,5 +1,6 @@
 idi <- function(y, ic=c("AIC","AICc","BIC","BICc"),
                 distribution1="drectnorm", distribution2="dbcnorm",
+                distribution3="dnorm",
                 distribution2Occurrence="plogis"){
 
     # Select IC
@@ -20,13 +21,18 @@ idi <- function(y, ic=c("AIC","AICc","BIC","BICc"),
     xreg2Occurrence$y[] <- (xreg2Occurrence$y!=0)*1;
     xreg2Occurrence$x[] <- lowess(xreg2Occurrence$y)$y;
 
+    ### This is slow mover with fractional values
     # First model with rectified normal distribution
     model1 <- suppressWarnings(alm(y~., xreg1, distribution=distribution1));
 
+    ### Erratic demand (low volume with stockouts)
     # Model for demand occurrence
     model2Occurrence <- suppressWarnings(alm(y~., xreg2Occurrence, distribution=distribution2Occurrence));
     # The second model: mixture of Box-Cox Normal and Logistic
     model2 <- suppressWarnings(alm(y~., xreg2Sizes, distribution=distribution2, occurrence=model2Occurrence));
+
+    ### Regular demand
+    model3 <- suppressWarnings(alm(y~., xreg1, distribution=distribution3));
 
     # Fix the number of estimated parameters to include occurrence part
     # model2$df <- model2$df + nparam(model2$occurrence)
