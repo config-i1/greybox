49ebc629114d4a05a054fc2496145024fc71dae5
Ivan Svetunkov <ivan@svetunkov.com>
2024-12-09 16:59:15 +0000

    Support for palette in tableplot().

--- R/tableplot.R
+++ R/tableplot.R
@@ -4,8 +4,11 @@
 #'
 #' The function produces the plot of the \code{table()} function with colour densities
 #' corresponding to the respective frequencies of appearance. If the value appears more
-#' often than the other (e.g. 0.5 vs 0.15), then it will be darker. The frequency of 0
-#' corresponds to the white colour, the frequency of 1 corresponds to the black.
+#' often than the other (e.g. 0.5 vs 0.15), then it will be darker. By default, the
+#' frequency of 0 corresponds to the white colour, the frequency of 1 corresponds to the
+#' black. The colours can be changed by defining a different palette via the
+#' \code{palette()} function. The function also adds the number of dots (points)
+#' proportional to the nuber of values in each category to simplify the reading of plots.
 #'
 #' See details in the vignette "Marketing analytics with greybox":
 #' \code{vignette("maUsingGreybox","greybox")}
@@ -31,10 +34,11 @@
 #'
 #' @examples
 #'
+#' palette("Tableau")
 #' tableplot(mtcars$am, mtcars$gear)
 #'
 #' @importFrom utils head tail
-#' @importFrom grDevices colorRampPalette
+#' @importFrom grDevices colorRampPalette palette
 #' @export tableplot
 tableplot <- function(x, y=NULL, labels=TRUE, legend=FALSE, points=TRUE, ...){
     ellipsis <- list(...);
@@ -77,6 +81,7 @@ tableplot <- function(x, y=NULL, labels=TRUE, legend=FALSE, points=TRUE, ...){
 
     tableData <- tableDataInitial <- table(x,y);
     tableData[] <- tableData / sum(tableData);
+    obsInsample <- sum(tableDataInitial);
 
     # An option - make the same density or make it changable
     # tableDataColours <- 1 - 0.75 * tableData / max(tableData);
@@ -139,35 +144,55 @@ tableplot <- function(x, y=NULL, labels=TRUE, legend=FALSE, points=TRUE, ...){
         mar2 <- c(0,0,2.1,2.1);
     }
 
+    # If the default palette is used, define the new one in black and white
+    paletteBasic <- palette();
+    palette("default");
+    paletteDefault <- palette();
+    if(all(paletteBasic %in% paletteDefault) &&
+       all(paletteBasic==paletteDefault)){
+        paletteBasic <- c("white","black");
+    }
+    else{
+        palette(paletteBasic);
+    }
+
+    # Create gradients of different insensitivity
+    paletteBasicCol <- colorRampPalette(paletteBasic[1:2])(1000)[findInterval(t(tableData), seq(0, 1, length.out=1000))];
+
     if(legend){
         parDefault <- par(no.readonly=TRUE);
         on.exit(par(parDefault));
+        colPalette <- colorRampPalette(c(paletteBasic[1],paletteBasic[2]));
         layout(matrix(1:2,1,2),widths=c(0.9,0.1));
-        colPalette <- colorRampPalette(c("white","black"));
         par(mar=mar1);
     }
 
     do.call(plot, ellipsis);
+    k <- 1;
     for(i in 1:(length(xCoord)-1)){
         for(j in 1:(length(yCoord)-1)){
             polygon(c(c(xCoord[i],xCoord[i+1]),rev(c(xCoord[i],xCoord[i+1]))),
                     c(c(yCoord[j],yCoord[j]),c(yCoord[j+1],yCoord[j+1])),
-                    col=rgb(tableDataColours[i,j],tableDataColours[i,j],tableDataColours[i,j]));
+                    col=paletteBasicCol[k]);
+            k <- k+1;
+
+            # Colour for text and points
+            if(tableData[i,j]>0.5){
+                colAdditional <- paletteBasic[1];
+            }
+            else{
+                colAdditional <- paletteBasic[2];
+            }
             if(labels){
-                if(tableData[i,j]>0.5){
-                    textCol <- "white";
-                }
-                else{
-                    textCol <- "black";
-                }
-                text(xMid[i],yMid[j],labels=round(tableData[i,j],5),col=textCol);
+                text(xMid[i],yMid[j],labels=round(tableData[i,j],5),col=colAdditional);
             }
             if(points){
                 if(tableDataInitial[i,j]!=0){
                     # points(rep(xCoord[i]+0.1,tableDataInitial[i,j]),
                     #        yCoord[j]+0.1+c(1:tableDataInitial[i,j])/(tableDataInitial[i,j])*0.75)
                     points(rep(xCoord[i]+0.2,tableDataInitial[i,j]),
-                           yCoord[j]+0.5+c(1:tableDataInitial[i,j]-0.5-tableDataInitial[i,j]/2)/(tableDataInitial[i,j])*0.75)
+                           yCoord[j]+0.5+c(1:tableDataInitial[i,j]-0.5-tableDataInitial[i,j]/2)/(tableDataInitial[i,j])*0.75,
+                           col=colAdditional)
                 }
             }
         }
